15c1ae163fdbfc8f461281e4d1d5e8a3e14b45cd
Krystsina Ludanik <krystsynalud89@gmail.com>
2023-07-21 13:48:37 +0200

    Add 4 files

---
+++ tasks2.js
@@ -0,0 +1,9 @@
+//2//  Составить алгоритм: если введенное имя совпадает с Вячеслав,
+// то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"
+const data = "Any name";
+
+if (data === `Viacheslav`) {
+  console.log(`Hello,Viacheslav!`);
+} else {
+  console.log(`No such name!`);
+}
